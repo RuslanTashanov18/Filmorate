648e9019b9691e96df6050330fc5023451ce90b2
RuslanTashanov18 <tashanov.2019@gmail.com>
2026-03-27 15:29:53 +0500

    Create Exception for project

---
+++ src/main/java/com/example/Filmorate/exceptions/InvalidValidationException.java
@@ -0,0 +1,7 @@
+package com.example.Filmorate.exceptions;
+
+public class InvalidValidationException extends RuntimeException {
+    public InvalidValidationException(String message) {
+        super(message);
+    }
+}
